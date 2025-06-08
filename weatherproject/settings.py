9e63de4b6b81930d205faac1c3a77a4b326e8209
Desamsetti Sujatha <142564438+sujatha1216@users.noreply.github.com>
2025-06-08 20:55:12 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS= ["https://refreshing-perfection-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS= ["https://refreshing-perfection-production.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
